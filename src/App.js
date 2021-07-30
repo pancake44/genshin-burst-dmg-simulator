0ebc6062e27c6c106a5a5f09b32cc913077dde28
Harry <hvs0808@gmail.com>
2021-07-30 10:11:07 -0400

    v1.0 add loading screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Form from './Form.js';
 import Img from './Img.js';
 import images from './public/monsters/index.js';
 import HeaderBar from './HeaderBar.js';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 function getImg(){
 	var arr = ['a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k' ];
@@ -13,19 +14,40 @@ function getImg(){
 function App() {
 
 	const [img, setImg] = useState(getImg());
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 3000);
+	}, []);
 
 	return (
     	<div className="App">
 			<HeaderBar />
-			<div className="Container">
-				<div className="Item">
-					<Img img={img}/>
-				</div>
-				<div className="Item">
-					<Form setImg={setImg}/>
-				</div>
-			</div>
-   		</div>
+			{
+				loading ? ( 
+					<div style={{width: "100%", height: "100vh", alignItems: "center", justifyContent: "center", display: "flex"}}>
+						<ClipLoader
+							size={90}
+							color={"#123abc"}
+							loading={loading}
+						/>
+					</div>
+				)
+				: (
+					<div className="Container">
+						<div className="Item">
+							<Img img={img}/>
+						</div>
+						<div className="Item">
+							<Form setImg={setImg}/>
+						</div>
+					</div>
+				)
+			}
+	   	</div>
   );
 }
 
